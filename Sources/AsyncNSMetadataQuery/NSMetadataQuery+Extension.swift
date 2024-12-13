d726e4b45f575fb000f2f2945ccabd7d3fe37b11
Ryan Boyer <ryanjboyer@gmail.com>
2024-12-12 21:00:17 -0700

    Remove mapped notification names

--- Sources/AsyncNSMetadataQuery/NSMetadataQuery+Extension.swift
+++ Sources/AsyncNSMetadataQuery/NSMetadataQuery+Extension.swift
@@ -2,20 +2,6 @@ import Combine
 import Foundation
 
 public extension NSMetadataQuery {
-	/// Posted when the receiver’s results have changed during the live-update phase of the query.
-	nonisolated static var didUpdate: Notification.Name { .NSMetadataQueryDidUpdate }
-
-	/// Posted as the receiver is collecting results during the initial result-gathering phase of the query.
-	nonisolated static var gatheringProgress: Notification.Name { .NSMetadataQueryGatheringProgress }
-
-	/// Posted when the receiver begins with the initial result-gathering phase of the query.
-	nonisolated static var didStartGathering: Notification.Name { .NSMetadataQueryDidStartGathering }
-
-	/// Posted when the receiver has finished with the initial result-gathering phase of the query.
-	nonisolated static var didFinishGathering: Notification.Name { .NSMetadataQueryDidFinishGathering }
-}
-
-public extension NSMetadataQuery {
 	/// Asynchronously start the query and wait for results.
 	/// - Parameters:
 	///   - notificationCenter: The notification center used to wait for the ``NSMetadataQuery/didFinishGathering`` notification.
@@ -23,7 +9,7 @@ public extension NSMetadataQuery {
 		isolation: isolated (any Actor)? = #isolation,
 		notificationCenter: NotificationCenter = .default
 	) async {
-		let notificationName = NSMetadataQuery.didFinishGathering
+		let notificationName: Notification.Name = .NSMetadataQueryDidFinishGathering
 
 		await withCheckedContinuation { continuation in
 			var subscriber: AnyCancellable?
